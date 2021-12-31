6adb1a9a7ea73f4f14c0879c9be731c8b61235ec
Muukii <muukii.app@gmail.com>
2022-01-01 00:17:45 +0900

    Update

--- Sources/GeometryKit.swift
+++ Sources/GeometryKit.swift
@@ -6,7 +6,7 @@ public enum Geometry {
 
 extension Geometry {
 
-  public func sizeThatAspectFit(size: CGSize, maxPixelSize: CGFloat) -> CGSize {
+  public static func sizeThatAspectFit(size: CGSize, maxPixelSize: CGFloat) -> CGSize {
 
     guard size.width >= maxPixelSize || size.height >= maxPixelSize else {
       return size
@@ -28,7 +28,7 @@ extension Geometry {
     return s
   }
 
-  public func sizeThatAspectFit(aspectRatio: CGSize, boundingSize: CGSize) -> CGSize {
+  public static func sizeThatAspectFit(aspectRatio: CGSize, boundingSize: CGSize) -> CGSize {
     let widthRatio = boundingSize.width / aspectRatio.width
     let heightRatio = boundingSize.height / aspectRatio.height
     var size = boundingSize
@@ -45,7 +45,7 @@ extension Geometry {
     )
   }
 
-  public func sizeThatAspectFill(aspectRatio: CGSize, minimumSize: CGSize) -> CGSize {
+  public static func sizeThatAspectFill(aspectRatio: CGSize, minimumSize: CGSize) -> CGSize {
     let widthRatio = minimumSize.width / aspectRatio.width
     let heightRatio = minimumSize.height / aspectRatio.height
 
@@ -63,7 +63,8 @@ extension Geometry {
     )
   }
 
-  public func rectThatAspectFit(aspectRatio: CGSize, boundingRect: CGRect) -> CGRect {
+  /// Returns a rectangle that fits inside provided bounding rectangle with respecting aspect ratio.
+  public static func rectThatAspectFit(aspectRatio: CGSize, boundingRect: CGRect) -> CGRect {
     let size = sizeThatAspectFit(aspectRatio: aspectRatio, boundingSize: boundingRect.size)
     var origin = boundingRect.origin
     origin.x += (boundingRect.size.width - size.width) / 2.0
@@ -71,7 +72,8 @@ extension Geometry {
     return CGRect(origin: origin, size: size)
   }
 
-  public func rectThatAspectFill(aspectRatio: CGSize, minimumRect: CGRect) -> CGRect {
+  /// Returns a rectangle that fills inside provided bounding rectangle with respecting aspect ratio.
+  public static func rectThatAspectFill(aspectRatio: CGSize, minimumRect: CGRect) -> CGRect {
     let size = sizeThatAspectFill(aspectRatio: aspectRatio, minimumSize: minimumRect.size)
     var origin = CGPoint.zero
     origin.x = (minimumRect.size.width - size.width) / 2.0
@@ -79,7 +81,7 @@ extension Geometry {
     return CGRect(origin: origin, size: size)
   }
 
-  public func diagonalRatio(to: CGSize, from: CGSize) -> CGFloat {
+  public static func diagonalRatio(to: CGSize, from: CGSize) -> CGFloat {
     let _from = sqrt(pow(from.height, 2) + pow(from.width, 2))
     let _to = sqrt(pow(to.height, 2) + pow(to.width, 2))
 
