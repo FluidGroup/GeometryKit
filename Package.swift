b0f6f6a1a2ca0ed2cac27dbf4d64842e17316e4e
Muukii <muukii.app@gmail.com>
2022-03-27 20:53:59 +0900

    Add tests

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "GeometryKit",
-    platforms: [
-        .iOS(.v11),
-    ],
-    products: [
-        .library(name: "GeometryKit", targets: ["GeometryKit"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "GeometryKit",
-            dependencies: [],
-            path: "Sources"
-        )
-    ]
+  name: "GeometryKit",
+  platforms: [
+    .iOS(.v11)
+  ],
+  products: [
+    .library(name: "GeometryKit", targets: ["GeometryKit"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "GeometryKit",
+      dependencies: [],
+      path: "Sources"
+    ),
+    .testTarget(name: "GeometryKitTests", dependencies: ["GeometryKit"])
+  ]
 )
---
+++ Tests/GeometryKitTests/File.swift
@@ -0,0 +1,11 @@
+import XCTest
+
+import GeometryKit
+
+final class BasicTests: XCTestCase {
+  
+  func testBasic() {
+    
+  }
+  
+}
